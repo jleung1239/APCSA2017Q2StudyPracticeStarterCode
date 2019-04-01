5eb8bf47cf9f014e111b42093ec1105a17dc49a3
BT_1N3_13 <a>
2019-04-01 08:34:01 -0400

    a

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,12 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int first;
+    private int initial;
+    public class MultPractice(int first, int initial)
+    {
+        this.first=first;
+        this.initial=initial;
+    }
 }
