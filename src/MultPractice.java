db76bb4352a403e73921f9c200f2f2d88518d8ca
BT_1N3_13 <a>
2019-04-01 08:35:11 -0400

    a

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -4,9 +4,16 @@
 public class MultPractice implements StudyPractice{
     private int first;
     private int initial;
-    public class MultPractice(int first, int initial)
+    public MultPractice(int first, int initial)
     {
         this.first=first;
         this.initial=initial;
+    }
+    public String getProblem(){
+        String problem = first + " TIMES " +initial;
+        return problem;
+    }
+    public void nextProblem(){
+        initial++;
     }
 }
